0ed60dfd51221e14781e9210b8a696ea3bebdd4a
Medina Hasna Lucita <97207992+Medinahl28@users.noreply.github.com>
2022-03-20 21:10:32 +0700

    Create car.java
    This is a mini oop project named car, and this is the first part.
    
    I used Java as the language, VS code as the tool, and Encapsulation as the OOP pillar

---
+++ car.java
@@ -0,0 +1,39 @@
+public class car {
+    public String carName;
+    private String color;
+    private int price;
+    private String brand;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setbrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setcarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+}
